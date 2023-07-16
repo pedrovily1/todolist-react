76dcaf7d33ff11084b764fe05d725ab1281d9e34
pedro modesto <pedrovily67@gmail.com>
2023-07-16 18:11:17 -0400

    test

--- server.js
+++ server.js
@@ -4,53 +4,36 @@ import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000;
 
-//import { Pool } from 'pg';
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 const { Pool } = pg;
 const db_conn = new Pool({
   connectionString: process.env.DATABASE_URL,
-});
-
-app.use(express.static("dist"))
-
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'todos',
-  password: '4545',
-  port: 5432,
-});
-
-/*let todos = [
-    { id: 1, text: 'First todo', completed: false },
-    { id: 2, text: 'Second todo', completed: false },
-  ];
-*/
-  function generateId() {
-    return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+  ssl: {
+    rejectUnauthorized: false
   }
+});
 
- // Create
+app.use(express.static("dist"));
+
+// Create
 app.post('/api/todos', (req, res) => {
   const { text } = req.body;
-  const newTodo = {  text, completed: false };
+  const newTodo = { text, completed: false };
 
-  pool.query(
-    'INSERT INTO todos (text, completed) VALUES ($1, $2)',
+  db_conn.query(
+    'INSERT INTO todos (text, completed) VALUES ($1, $2) RETURNING *',
     [newTodo.text, newTodo.completed],
-    (err) => {
+    (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).json({ message: 'Internal server error' });
       } else {
-        res.json(newTodo);
+        res.json(result.rows[0]);
       }
     }
   );
@@ -58,7 +41,7 @@ app.post('/api/todos', (req, res) => {
 
 // Read
 app.get('/api/todos', (req, res) => {
-  pool.query('SELECT * FROM todos', (err, result) => {
+  db_conn.query('SELECT * FROM todos', (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).json({ message: 'Internal server error' });
@@ -72,7 +55,7 @@ app.get('/api/todos', (req, res) => {
 app.get('/api/todos/:id', (req, res) => {
   const { id } = req.params;
 
-  pool.query(
+  db_conn.query(
     'SELECT * FROM todos WHERE id = $1',
     [id],
     (err, result) => {
@@ -93,16 +76,15 @@ app.put('/api/todos/:id', (req, res) => {
   const { id } = req.params;
   const { text, completed } = req.body;
 
-  pool.query(
-    'UPDATE todos SET text = $1, completed = $2 WHERE id = $3',
+  db_conn.query(
+    'UPDATE todos SET text = $1, completed = $2 WHERE id = $3 RETURNING *',
     [text, completed, id],
     (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).json({ message: 'Internal server error' });
-      } else if (result.rowCount > 0) {
-        const updatedTodo = { id, text, completed };
-        res.json(updatedTodo);
+      } else if (result.rows.length > 0) {
+        res.json(result.rows[0]);
       } else {
         res.status(404).json({ message: 'Todo not found' });
       }
@@ -114,7 +96,7 @@ app.put('/api/todos/:id', (req, res) => {
 app.delete('/api/todos/:id', (req, res) => {
   const { id } = req.params;
 
-  pool.query(
+  db_conn.query(
     'DELETE FROM todos WHERE id = $1',
     [id],
     (err, result) => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 function App() {
-  const todos = [
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Build a Todo App', completed: false },
-    { id: 3, text: 'Deploy to production', completed: false }
-  ];
+  const [todos, setTodos] = useState([]);
+  const [newTodoText, setNewTodoText] = useState('');
+  const [editingId, setEditingId] = useState(null);
+
+  useEffect(() => {
+    fetch('/api/todos')
+      .then(response => response.json())
+      .then(data => setTodos(data));
+  }, []);
+
+  function addTodo() {
+    const newTodo = { text: newTodoText };
+
+    fetch('/api/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTodos(prevTodos => [...prevTodos, data]);
+        setNewTodoText('');
+      });
+  }
+
+  function deleteTodo(id) {
+    fetch(`/api/todos/${id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+      });
+  }
+
+  function updateTodo(id, text, completed) {
+    const updatedTodo = { text, completed };
+
+    fetch(`/api/todos/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedTodo),
+    })
+      .then(response => response.json())
+      .then(data => {
+        setTodos(prevTodos => prevTodos.map(todo => todo.id === id ? data : todo));
+      });
+  }
 
   return (
     <div className="todo-container">
       <h1>Todo List</h1>
-      <input type="text" value="" />
-      <button>Add todo</button>
+      <input
+        type="text"
+        value={newTodoText}
+        onChange={e => setNewTodoText(e.target.value)}
+      />
+      <button onClick={addTodo}>Add todo</button>
 
       <ul>
         {todos.map(todo => (
           <li key={todo.id}>
-            <span>{todo.text}</span>
-            <input type="checkbox" checked={todo.completed} />
-            <button>Edit</button>
-            <button>Delete</button>
+            {editingId === todo.id ? (
+              <input
+                type="text"
+                value={todo.text}
+                onChange={e => updateTodo(todo.id, e.target.value, todo.completed)}
+              />
+            ) : (
+              <span>{todo.text}</span>
+            )}
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={e => updateTodo(todo.id, todo.text, e.target.checked)}
+            />
+            <button onClick={() => setEditingId(editingId === todo.id ? null : todo.id)}>
+              {editingId === todo.id ? 'Done' : 'Edit'}
+            </button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
           </li>
         ))}
       </ul>
